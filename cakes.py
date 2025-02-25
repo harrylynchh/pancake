4235a23592f83f1aef3adb9108fcedfeca883716
Harry Lynch <hlynch02@tufts.edu>
2025-02-25 02:31:40 -0500

    Thinking...

--- cakes.py
+++ cakes.py
@@ -1,16 +1,9 @@
 import random
 class PancakeStack:
     stack: list
-    def __init__(self, size: int):
-        self.stack = list(range(1,size + 1))
-        random.shuffle(self.stack)
-    def flip(self, n: int) -> None:
-        if n > len(self.stack) or n <= 0:
-            print("ERR: OOB")
-            return
-        start = 0
-        end = n - 1
-        while start < end:
-            self.stack[start], self.stack[end] = self.stack[end], self.stack[start]
-            start += 1
-            end -= 1
+    heuristic: int
+    cost: int
+    def __init__(self, stk: list):
+        self.stack = list(stk)
+        self.heuristic = 0;
+        self.cost = 0;
--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import random
 from globals import *
 from cakes import PancakeStack
+SIZE = 10
+stack = list(range(1,SIZE + 1))
+random.shuffle(stack)
+def flip(stack: list, n: int) -> list:
+        newStack = list(stack);
+        if n > len(newStack) or n <= 0:
+            print("ERR: OOB")
+            return
+        start = 0
+        end = n - 1
+        while start < end:
+            newStack[start], newStack[end] = newStack[end], newStack[start]
+            start += 1
+            end -= 1
+        return newStack
 pancakes = PancakeStack(10)
 print(pancakes.stack)
 pancakes.flip(6)
